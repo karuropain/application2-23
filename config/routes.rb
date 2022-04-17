72dc198ce25f030c6aa52951e86085265849fb95
karuropain <fs.sai5041@gmail.com>
2022-04-17 15:15:34 +0000

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ root to: "homes#top"
 
 get 'homes/about' => 'homes#about', as: :about
 
-resources :post_images, only: [:new, :create, :index, :show]
+resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
 end
