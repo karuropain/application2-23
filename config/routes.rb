8d922e359c664cfb369d390122a08b960a2233e8
karuropain <fs.sai5041@gmail.com>
2022-04-20 14:34:04 +0000

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ root to: "homes#top"
 
 get 'homes/about' => 'homes#about', as: :about
 
-resources :post_images, only: [:new, :create, :index, :show, :destroy]
+resources :post_images, only: [:new, :index, :show]#create,destroyを14章で消した
+resources :users, only: [:show, :edit]
 
 end
